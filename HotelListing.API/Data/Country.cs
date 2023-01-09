459b3a7b67e5de74e93fddc589745989b201f3bf
Matthew Baier <Matthew.Baier@samaritanministries.org>
2023-01-09 14:07:27 -0600

    Added countries Endpoint

--- HotelListing.API/Data/Country.cs
+++ HotelListing.API/Data/Country.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; }
         public string ShortName { get; set; }
 
-        public virtual ICollection<Hotel> Hotels { get; set; }
+        public virtual ICollection<Hotel>? Hotels { get; set; }
     }
 }
---
+++ HotelListing.API/Migrations/20230106145812_MadeICollectionOfHotelsNullable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    public partial class MadeICollectionOfHotelsNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
